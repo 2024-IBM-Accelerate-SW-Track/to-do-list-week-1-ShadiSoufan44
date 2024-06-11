0a47b1c10028f012a44345f3fd93979c0771d953
ShadiSoufan44 <shadi_soufan@brown.edu>
2024-06-10 22:54:17 -0400

    Passing on IDE

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
+import profilePic from "../assets/profile_pic_name.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img className="profile_image" profilePic alt="Profile Pic"></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Your Name</div>
+              <div className="brief_description">
+                Hi! My name is Shadi Soufan and I'm a rising junior at Brown
+                University studying Computer Science and Economics! In my free
+                time, I enjoy playing basketball, hanging out with family and
+                friends, and traveling.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
